6bc6aff99f2fb57cf62762bc68cfb9b3bccb029b
paizv <paizv@rose-hulman.edu>
2019-01-23 09:09:54 -0500

    DONE

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Valeria Paiz.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -46,7 +46,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ###########################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -126,7 +126,7 @@ def run_test_practice_problem3():
     st.SimpleTestCase.run_tests('practice_problem3', tests)
 
     ###########################################################################
-    # TO DO 2 continued:  More tests:
+    # DONE 2 continued:  More tests:
     #      YOU add at least **   2   ** additional tests here.
     #
     # You can use the   SimpleTestCase  class as above, or use
@@ -135,6 +135,15 @@ def run_test_practice_problem3():
     # SUGGESTION: Ask an assistant to CHECK your tests to confirm
     #             that they are adequate tests!
     ###########################################################################
+    expected = [0, 1, 2, 6]
+    actual = practice_problem3(0, 4, 0.45)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    expected = [6, 7, 8, 12, 13, 14, 19, 20, 21,25]
+    actual = practice_problem3(3, 10, 0.1)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 
 def practice_problem3(start, n, threshold):
@@ -209,14 +218,26 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ###########################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ###########################################################################
-    # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
+    # DIFFICULTY AND TIME RATINGS (see top of his file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ###########################################################################
+    list_nums = []
+    k = start
+    count = 0
+    while True:
+        if count >= n:
+            break
+        if (math.cos(k) + math.sin(k)) > threshold:
+            list_nums.append(k)
+            count = count + 1
+        k = k + 1
+
+    return list_nums
 
 
 # -----------------------------------------------------------------------------
